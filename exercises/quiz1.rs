f7a41eb7e435810d31718b9b096960f398c08028
Pavel Fomchenkov <hello@pavel.codes>
2023-03-12 20:06:28 +0100

    Complete Quiz 2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_count: usize) -> usize {
+    let apple_price = if apple_count > 40 { 1 } else { 2 };
+    apple_price * apple_count
+}
 
 // Don't modify this function!
 #[test]
